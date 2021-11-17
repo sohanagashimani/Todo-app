f05b059bf538112979c303080daacf47e096c821
Sohan Agashimani <sohanagashimanii@gmail.com>
2021-11-17 14:19:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Todo-app
+ A simple todo app.
---
+++ index.js
@@ -0,0 +1,50 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUl = document.getElementById("todos");
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
+function addTodo(todo) {
+  let todoText = input.value;
+  if (todo) {
+    todoText = todo.text;
+  }
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+    todoEl.innerText = todoText;
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLs();
+    });
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+      updateLs();
+    });
+    todosUl.appendChild(todoEl);
+    input.value = "";
+    updateLs();
+  }
+}
+
+function updateLs() {
+  const todoslis = document.querySelectorAll("li");
+  const todos = [];
+  todoslis.forEach((todo) => {
+    todos.push({
+      text: todo.innerText,
+      completed: todo.classList.contains("completed"),
+    });
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
